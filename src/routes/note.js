135ae50b44d105536ff62c51b261aaf18963fe3b
KaviiSuri <surikavii@gmail.com>
2021-02-13 11:59:24 +0530

    completed note

--- src/routes/note.js
+++ src/routes/note.js
@@ -2,10 +2,9 @@ const router = require("express").Router();
 const asyncHandler = require("express-async-handler");
 const path = require("path");
 const thisFile = path.basename(__filename);
-const Note=require("../models/Note");
-const Scroll=require("../models/Scroll");
+const Note = require("../models/Note");
+const Scroll = require("../models/Scroll");
 const { isAuthenticated } = require("../middlewares/auth");
-
 
 router.get("/test", (req, res) => {
   res.json({ success: true, message: `Router ${thisFile}  Working!` });
@@ -21,23 +20,23 @@ router.post(
       err.name = "BadRequest";
       throw err;
     }
-    
+
     const scroll = await Scroll.findById(req.body.scroll_id);
     if (!scroll) {
       err = new Error("Scroll Not Found");
       err.statusCode = 404;
       err.name = "NotFound";
-      throw err; 
+      throw err;
     }
     //create notes
     const note = await Note.create({
       name: req.body.name,
-      scrolls:req.body.scroll_id,
-      // start_time: 
+      scroll: req.body.scroll_id,
+      start_time: req.body.start_time,
       content: req.body.content,
     });
-    const scroll = await Scroll.findById(req.body.scroll_id);
-    scrolls.notes.push(note._id);
+    // const scroll = await Scroll.findById(req.body.scroll_id);
+    scroll.notes.push(note._id);
     await scrolls.save();
     res.status(201).json(note);
   })
@@ -64,14 +63,15 @@ router.delete(
   isAuthenticated,
   asyncHandler(async (req, res) => {
     // query 1 - find notebook
-    const note = Note.findById(req.params._id);
+    const note = await Note.findById(req.params._id);
     if (!note) {
       err = new Error("Note Not Found");
       err.statusCode = 404;
       err.name = "NotFound";
       throw err;
     }
-  
+
+    const scroll = await Scroll.findById(note.scroll);
     if (scroll.owner.toString() !== req.user._id.toString()) {
       err = new Error("Not Authorized to delete it");
       err.statusCode = 401;
@@ -79,8 +79,7 @@ router.delete(
       throw err;
     }
     // query 2 - remvoe from parent scroll
-    const scroll = Scroll.findById(note.scroll);
-    scroll.notes = scroll.notes.filter(
+    scroll.notes = await scroll.notes.filter(
       (id) => id.toString() !== note._id.toString()
     );
     await scroll.save();
@@ -91,5 +90,38 @@ router.delete(
   })
 );
 
+router.patch(
+  "/:_id",
+  isAuthenticated,
+  asyncHandler(async (req, res) => {
+    const { name, content, start_time } = req.body;
+    const note = await Note.findById(req.params._id);
+    if (!note) {
+      err = new Error("Note Not Found");
+      err.statusCode = 404;
+      err.name = "NotFound";
+      throw err;
+    }
+
+    const scroll = await Scroll.findById(note.scroll);
+    if (scroll.owner.toString() !== req.user._id.toString()) {
+      err = new Error("Not Authorized to edit it");
+      err.statusCode = 401;
+      err.name = "AuthError";
+      throw err;
+    }
+    if (name && name.length > 0) {
+      note.name = name;
+    }
+    if (content) {
+      note.content = content;
+    }
+    if (start_time) {
+      note.start_time = start_time;
+    }
+    await note.save();
+    res.status(200).json(note);
+  })
+);
 
 module.exports = router;
